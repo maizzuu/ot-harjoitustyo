4dd107381c521e9c94b09ed54c1f62fed4f6e7eb
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-05-03 16:04:59 +0300

    added docstring

--- src/entities/month.py
+++ src/entities/month.py
@@ -1,5 +1,35 @@
 class Month:
+    """Class with which the user can keep track of their spending.
+
+    Spending is logged in six different categories.
+
+    Attributes:
+        username: Name of the user who the month belongs to.
+        month: Month
+        year: Year
+        Categories:
+        food: Food + other essentials
+        living: Living
+        hobbies: Hobbies
+        transportation: Transportation
+        culture: Culture + restaurants
+        other: Everything else
+    """
+
     def __init__(self, username, month, year, food, living, hobbies, transportation, culture, other):
+        """Class constructor that creates a new month.
+
+        Args:
+            username: Name of the user who owns this month.
+            month: Month
+            year: Year
+            food: Amount spent on food.
+            living: Amount spent on living.
+            hobbies: Amount spent on hobbies.
+            transportation: Amount spent on transportation.
+            culture: Amount spent on culture.
+            other: Amount spent on other things.
+        """
         self.username = username
         self.month = month
         self.year = year
--- src/entities/user.py
+++ src/entities/user.py
@@ -1,5 +1,16 @@
-
 class User:
-    def __init__(self, username, password):
+    """Class that depicts a User.
+
+    Attributes:
+        username: String that represents the users username.
+        password: String that represents the users password.
+    """
+    def __init__(self, username:str, password:str):
+        """Class constructor that creates a new user.
+
+        Args:
+            username (str): String that represents the users username.
+            password (str): String that represents the users password.
+        """
         self.username = username
         self.password = password
