b06b0e06fb51279a10f27fea11801bd46a0b584f
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-05-13 16:07:48 +0300

    added parameter

--- src/ui/user_interface.py
+++ src/ui/user_interface.py
@@ -1,6 +1,7 @@
 from tkinter import Tk, ttk
 from ui.login_ui import LoginUI
 from ui.months_ui import MonthUI
+from ui.create_ui import CreateUserUI
 
 class UI:
     def __init__(self, root):
@@ -19,7 +20,7 @@ class UI:
     def _show_login(self):
         self._hide_current_view()
 
-        self._current_view = LoginUI(self._root, self._show_months)
+        self._current_view = LoginUI(self._root, self._show_months, self._show_create_view)
 
         self._current_view.pack()
 
@@ -27,5 +28,12 @@ class UI:
         self._hide_current_view()
 
         self._current_view = MonthUI(self._root, self._show_login)
+
+        self._current_view.pack()
+
+    def _show_create_view(self):
+        self._hide_current_view()
+
+        self._current_view = CreateUserUI(self._root, self._show_months, self._show_login)
 
         self._current_view.pack()
