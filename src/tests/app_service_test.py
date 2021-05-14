beb37f92859f64fd3f912cab7d97db1396a2128b
Maija Pajumaa <maija.pajumaa@helsinki.fi>
2021-05-14 14:16:43 +0300

    added tests

--- src/tests/app_service_test.py
+++ src/tests/app_service_test.py
@@ -24,9 +24,20 @@ class FakeMonthRepo:
         return month
 
     def spend(self, month, category, amount):
-        for m in self._months:
-            if m.username==month.username and m.month==month and m.year==year:
-                m.category += int(amount)
+        for item in self.months:
+            if item.username==month.username and item.month==month.month and item.year==month.year:
+                if category == "food":
+                    item.food += int(amount)
+                elif category == "living":
+                    item.living += int(amount)
+                elif category == "hobbies":
+                    item.hobbies += int(amount)
+                elif category == "transportation":
+                    item.transportation += int(amount)
+                elif category == "culture":
+                    item.culture += int(amount)
+                elif category == "other":
+                    item.other += int(amount)
                 break
         
     def delete_all(self):
@@ -41,7 +52,7 @@ class FakeUserRepo:
     
     def find_by_username(self, username):
         users = list(filter(lambda u: u.username==username, self.users))
-        return users[0]
+        return users[0] if len(users)>0 else None
 
     def create(self, user):
         self.users.append(user)
@@ -55,7 +66,7 @@ class TestAppService(unittest.TestCase):
         self.app_service = AppService(FakeMonthRepo(), FakeUserRepo())
 
         self.mikko = User("Mikko", "salis")
-        self.may = Month("Mikko", "May", "2021", 10, 0, 0, 0, 0, 0)
+        self.may = Month("Mikko", "May", "2021", 15, 0, 0, 0, 0, 0)
 
         self.app_service.create(self.mikko.username, self.mikko.password)
 
@@ -67,12 +78,41 @@ class TestAppService(unittest.TestCase):
     def test_login_user_invalid(self):
         self.assertRaises(InvalidInputError, lambda: self.app_service.login("invalid", "password"))
 
+    def test_logout(self):
+        self.app_service.login(self.mikko.username, self.mikko.password)
+
+        self.app_service.logout()
+
+        user = self.app_service.current_user()
+
+        self.assertEqual(user, None)
+
+    def test_current_user(self):
+        self.app_service.login(self.mikko.username, self.mikko.password)
+
+        user = self.app_service.current_user()
+
+        self.assertEqual(user.username, self.mikko.username)
+
+
     def test_get_months(self):
         self.app_service.login(self.mikko.username, self.mikko.password)
 
-        self.app_service.log(self.may.month, self.may.year, "", 0)
+        self.app_service.log(self.may.month, self.may.year, "food", self.may.food)
 
         months = self.app_service.get_months()
 
         self.assertEqual(len(months), 1)
-        self.assertEqual(months[1].food, self.may.food)
+        self.assertEqual(months[0].food, self.may.food)
+
+    def test_log(self):
+        self.app_service.login(self.mikko.username, self.mikko.password)
+
+        self.app_service.log(self.may.month, self.may.year, "living", 400)
+
+        month = self.app_service.get_months()[0]
+
+        self.assertEqual(month.living, 400)
+
+    def test_create_user_invalid(self):
+        self.assertRaises(UsernameExistsError, lambda: self.app_service.create(self.mikko.username, self.mikko.password))
